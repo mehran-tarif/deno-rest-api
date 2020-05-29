308aeb64e1147fb668324f7986061ba3b85a6bd9
Mehran Tarif <mrmntf@riseup.net>
2020-05-29 22:26:19 +0430

    part two

--- controllers.js
+++ controllers.js
@@ -1,4 +1,5 @@
 import { articles } from './models.js'
+import { writeJsonSync } from "https://deno.land/std/fs/mod.ts";
 
 const articlesList = (context) => {
 	context.response.body = articles
@@ -15,4 +16,93 @@ const articleDetail = (context) => {
 	}
 }
 
-export { articlesList, articleDetail }
+const articleAdd = async (context) => {
+	const body = await context.request.body()
+	if (body.type == "json") {
+		const value = body.value
+		value.id = articles.slice(-1)[0].id + 1
+
+		if (value.title && value.content) {
+
+			articles.push(value)
+			writeJsonSync("./db.json", articles, { spaces: 4 })
+			context.response.status = 201
+			context.response.body = value
+
+		} else {
+			context.response.status = 400
+			context.response.body = `"title" and "content" can't be empty.`
+		}
+
+	} else {
+		context.response.status = 400
+		context.response.body = `Data should be in Json.`
+	}
+}
+
+const articleUpdate = async (context) => {
+	const article = articles.find(a => a.id == context.params.id)
+
+	if (article) {
+
+		const body = await context.request.body()
+		if (body.type == "json") {
+			const value = body.value
+			value.id = article.id
+
+			if (value.title && value.content) {
+
+				for (var i = articles.length - 1; i >= 0; i--) {
+					if (articles[i].id == article.id) {
+						articles[i] = value
+						break
+					}
+				}
+
+				writeJsonSync("./db.json", articles, { spaces: 4 })
+				context.response.status = 200
+				context.response.body = value
+
+			} else {
+				context.response.status = 400
+				context.response.body = `"title" and "content" can't be empty.`
+			}
+
+		} else {
+			context.response.status = 400
+			context.response.body = `Data should be in Json.`
+		}
+
+	} else {
+		context.response.status = 404
+		context.response.body = `Article with id: ${context.params.id} not found.`
+	}
+}
+
+const articleDelete = (context) => {
+	const article = articles.find(a => a.id == context.params.id)
+
+	if (article) {
+		for (var i = articles.length - 1; i >= 0; i--) {
+			if (articles[i].id == article.id) {
+				articles.splice(i, 1)
+				break
+			}
+		}
+
+		writeJsonSync("./db.json", articles, { spaces: 4 })
+		context.response.status = 200
+		context.response.body = `Article with id: ${context.params.id} removed.`
+	} else {
+		context.response.status = 404
+		context.response.body = `Article with id: ${context.params.id} not found.`
+	}
+}
+
+export {
+	articlesList,
+	articleDetail,
+	articleAdd,
+	articleUpdate,
+	articleDelete
+}
--- routes.js
+++ routes.js
@@ -1,9 +1,18 @@
 import { Router } from "https://deno.land/x/oak/mod.ts";
-import { articlesList, articleDetail } from './controllers.js'
+import {
+	articlesList,
+	articleDetail,
+	articleAdd,
+	articleUpdate,
+	articleDelete
+} from './controllers.js'
 
 const router = new Router();
 router
 	.get("/", articlesList)
 	.get("/:id", articleDetail)
+	.post("/", articleAdd)
+	.put("/:id", articleUpdate)
+	.delete("/:id", articleDelete)
 
 export { router }
